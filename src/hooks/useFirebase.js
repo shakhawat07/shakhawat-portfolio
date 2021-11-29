dccd1181aa334b4136eaed126f69f484f48c8702
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-11-29 22:57:14 +0600

    first commit

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,136 @@
+// import { useState, useEffect } from 'react';
+// import React from 'react';
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, updateProfile, getIdToken, signOut } from "firebase/auth";
+import initializeAuthentication from '../FireBase/firebase.init';
+
+initializeAuthentication();
+
+const useFirebase = () => {
+    // const [user, setUser] = useState({});
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [authError, setAuthError] = useState('');
+    // const [admin, setAdmin] = useState(false);
+    // const [token, setToken] = useState('');
+
+    // const auth = getAuth();
+    // const googleProvider = new GoogleAuthProvider();
+
+    // const registerUser = (email, password, name, role, history) => {
+    //     setIsLoading(true);
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //         .then((userCredential) => {
+    //             setAuthError('');
+    //             const newUser = { email, displayName: name, role: role };
+    //             setUser(newUser);
+    //             // save user to the database
+    //             saveUser(email, name, role, 'POST');
+    //             // send name to firebase after creation
+    //             updateProfile(auth.currentUser, {
+    //                 displayName: name
+    //             }).then(() => {
+    //             }).catch((error) => {
+    //             });
+    //             history.replace('/');
+    //         })
+    //         .catch((error) => {
+    //             setAuthError(error.message);
+    //             console.log(error);
+    //         })
+    //         .finally(() => setIsLoading(false));
+    // }
+
+    // const loginUser = (email, password, location, history) => {
+    //     setIsLoading(true);
+    //     signInWithEmailAndPassword(auth, email, password)
+    //         .then((userCredential) => {
+    //             alert('Login successfully!');
+    //             const destination = location?.state?.from || '/';
+    //             history.replace(destination);
+    //             setAuthError('');
+    //         })
+    //         .catch((error) => {
+    //             setAuthError(error.message);
+    //             alert('Mismatched email or password. Login Error ' + { authError });
+    //             window.location.reload(false);
+    //         })
+    //         .finally(() => setIsLoading(false));
+    // }
+
+    // const signInWithGoogle = (location, history) => {
+    //     setIsLoading(true);
+    //     signInWithPopup(auth, googleProvider)
+    //         .then((result) => {
+    //             const user = result.user;
+    //             saveUser(user.email, user.displayName, 'PUT');
+    //             setAuthError('');
+    //             const destination = location?.state?.from || '/';
+    //             history.replace(destination);
+    //         }).catch((error) => {
+    //             setAuthError(error.message);
+    //         }).finally(() => setIsLoading(false));
+    // }
+
+    // // observer user state
+    // useEffect(() => {
+    //     const unsubscribed = onAuthStateChanged(auth, (user) => {
+    //         if (user) {
+    //             setUser(user);
+    //             getIdToken(user)
+    //                 .then(idToken => {
+    //                     // console.log(idToken);
+    //                     setToken(idToken);
+    //                 })
+    //         } else {
+    //             setUser({})
+    //         }
+    //         setIsLoading(false);
+    //     });
+    //     return () => unsubscribed;
+    // }, [auth, user, admin])
+
+    // useEffect(() => {
+    //     fetch(`https://hidden-ridge-10259.herokuapp.com/users/${user?.email}`)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log('firebaseData', data);
+    //             setAdmin(data?.admin);
+    //             console.log('firebase', admin);
+    //         })
+    // }, [user?.email, admin])
+
+    // const logout = () => {
+    //     setIsLoading(true);
+    //     signOut(auth).then(() => {
+    //         // Sign-out successful.
+    //     }).catch((error) => {
+    //         // An error happened.
+    //     })
+    //         .finally(() => setIsLoading(false));
+    // }
+
+    // const saveUser = (email, displayName, role, method) => {
+    //     const user = { email, displayName, role };
+    //     fetch('https://hidden-ridge-10259.herokuapp.com/users', {
+    //         method: method,
+    //         headers: {
+    //             'content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(user)
+    //     })
+    //         .then()
+    // };
+    return {
+        // user,
+        // admin,
+        // token,
+        // isLoading,
+        // authError,
+        // registerUser,
+        // loginUser,
+        // signInWithGoogle,
+        // logout,
+
+    }
+}
+
+export default useFirebase;
